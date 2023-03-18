eb56745bf5e201e8f3ae63e98c65d352d68d9c8e
rhl120 <rhl120@protonmail.com>
2023-03-18 10:55:20 +0100

    Add rook promotion

--- rhchess_web-a4c9839422398676.js
+++ rhchess_web-a4c9839422398676.js
@@ -639,7 +639,7 @@ function initSync(module) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('rhchess_web-22473ad42da34a5f_bg.wasm', import.meta.url);
+        input = new URL('rhchess_web-a4c9839422398676_bg.wasm', import.meta.url);
     }
     const imports = getImports();
 
