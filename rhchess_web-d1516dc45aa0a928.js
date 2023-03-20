fa57e85cc2d997a04bef8e39e18f6080285edd2b
rhl120 <rhl120@protonmail.com>
2023-03-20 19:02:12 +0100

    Fix promotion menu ui error caused by the new piece set

--- rhchess_web-d1516dc45aa0a928.js
+++ rhchess_web-d1516dc45aa0a928.js
@@ -639,7 +639,7 @@ function initSync(module) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('rhchess_web-a4c9839422398676_bg.wasm', import.meta.url);
+        input = new URL('rhchess_web-d1516dc45aa0a928_bg.wasm', import.meta.url);
     }
     const imports = getImports();
 
